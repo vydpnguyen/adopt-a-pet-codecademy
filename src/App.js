6dd9dfd7278efd9bf06f810e817d66f6d9081f8d
Vy Nguyen <vydpnguyen@gmail.com>
2023-07-30 22:12:55 +0700

    Initial set up

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
-
-// Add react-router-dom imports
+import { RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
 const appRouter = "REPLACE ME";
 
 function App() {
   return (
-    // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router={appRouter}/>
   );
 }
 
