08b9342296102799a3562871ae13318e0ceb0bae
Vy Nguyen <vydpnguyen@gmail.com>
2023-07-31 19:08:31 +0700

    Add nested routes to render Home Page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import { RouterProvider } from 'react-router-dom';
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
-const appRouter = createBrowserRouter(createRoutesFromElements(<Route path='/' element={ <Root/> }/>));
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={ <Root/> }>
+    <Route index element={ <HomePage/> }/>
+  </Route>
+));
 
 function App() {
   return (
