bc709ffafaedfe4a690db5e0853fba8424069a21
Vy Nguyen <vydpnguyen@gmail.com>
2023-07-31 20:16:42 +0700

    Update NavLink and Link

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const appRouter = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={ <Root/> }>
     <Route index element={ <HomePage/> } />
     <Route path=':type' element={ <HomePage /> }/>
+    <Route path=':type/:id' element={ <PetDetailsPage /> }/>
   </Route>
 ));
 
